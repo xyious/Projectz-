5725e3b5589c1a1553b3681c26c24bfb8df957c4
Alexander Knopf <xyious@gmail.com>
2016-06-07 01:02:41 -0500

    rails generate model Project name:string description:text deadline:datetime status:integer customer:string

---
+++ db/migrate/20160607054501_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[5.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.datetime :deadline
+      t.integer :status
+      t.string :customer
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601062556) do
+ActiveRecord::Schema.define(version: 20160607054501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "deadline"
+    t.integer  "status"
+    t.string   "customer"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
