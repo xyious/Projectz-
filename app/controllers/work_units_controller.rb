dd1324578a04dc553e20457aef3356f6ec7b1947
Alexander Knopf <xyious@gmail.com>
2016-06-14 06:36:07 -0500

    generate controller WorkUnits

---
+++ app/controllers/work_units_controller.rb
@@ -0,0 +1,2 @@
+class WorkUnitsController < ApplicationController
+end
---
+++ test/controllers/work_units_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WorkUnitsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
