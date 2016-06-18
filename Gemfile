c144b92d6411ebb426a5975f6fa0700567af4db3
Alexander Knopf <xyious@gmail.com>
2016-06-18 11:49:16 -0500

    static pages

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'turbolinks', '~> 5.x'
 gem 'jbuilder', '~> 2.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'bootstrap-sass',       '3.2.0.0'
 
 group :development do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,15 +2,21 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get "/"
     assert_response :success
     assert_select "title", "Projectz! Home"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get "/help"
     assert_response :success
     assert_select "title", "Projectz! Help"
+  end
+
+  test "should get contact" do
+    get "/contact"
+    assert_response :success
+    assert_select "title", "Projectz! Contact"
   end
 
 end
