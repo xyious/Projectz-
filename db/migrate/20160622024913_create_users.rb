ba0307a863568b090a89eb249c59ef80bba09c43
Alexander Knopf <xyious@gmail.com>
2016-06-21 21:50:28 -0500

    created users

---
+++ db/migrate/20160622024913_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin
+
+      t.timestamps
+    end
+    add_index :users, :email
+  end
+end
